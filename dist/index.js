b5a5f1bdfdbdf06fbf26b7b5c22e9467029641fe
Jan Küster <jkuester@uni-bremen.de>
2018-10-03 14:31:37 +0200

    initial build infrastructure created

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var _xmlJs = _interopRequireDefault(require("xml-js"));
+
+var _fs = _interopRequireDefault(require("fs"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var cwd = global.process.cwd();
+
+function xmi2json(path) {
+  var xml = _fs.default.readFileSync(path, 'utf8');
+
+  var options = {
+    compact: true,
+    spaces: 2
+  };
+  return _xmlJs.default.xml2json(xml, options);
+}
+
+function createJson(sourcePath, targetPath) {
+  var json = xmi2json(sourcePath);
+  return _fs.default.writeFileSync(targetPath, json, {
+    encoding: 'utf-8'
+  });
+} // Primitivetypes
+
+
+createJson("".concat(cwd, "/resources/PrimitiveTypes.xmi"), "".concat(cwd, "/resources/PrimitiveTypes.json"));
+createJson("".concat(cwd, "/resources/StandardProfile.xmi"), "".concat(cwd, "/resources/StandardProfile.json"));
+createJson("".concat(cwd, "/resources/UML.xmi"), "".concat(cwd, "/resources/UML.json"));
+createJson("".concat(cwd, "/resources/UMLDI.xmi"), "".concat(cwd, "/resources/UMLDI.json"));
+//# sourceMappingURL=index.js.map
